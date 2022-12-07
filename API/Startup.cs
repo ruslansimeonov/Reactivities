c315a4260bdf7c6ecf0894b008b093598737807a
Ruslan Simeonov <ruslansimenov@gmail.com>
2022-12-07 16:32:46 +0100

    Added front end skeleton. Calling api to get data.

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,13 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
 
         }
 
@@ -44,6 +51,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
