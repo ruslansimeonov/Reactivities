0da8b9ca9616267400a988adf7531d418044c448
Ruslan Simeonov <ruslansimenov@gmail.com>
2023-01-09 16:44:01 +0100

    Added Front End Functionality for editing an Activity

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Container } from 'semantic-ui-react';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data);
+      })
+  }, [])
+
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id))
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  return (
+    <Fragment>
+      <NavBar openForm={handleFormOpen}/>
+      <Container style={{ marginTop: '7em' }}>
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+        />
+      </Container>
+    </Fragment >
+  );
+}
+export default App;
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'semantic-ui-css/semantic.min.css'
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import App from './app/layout/App';
+import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <App />
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
